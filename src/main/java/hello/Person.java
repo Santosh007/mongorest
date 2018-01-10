85552b6ad248d43852f902b33bdf607b45bb1376
santosh007 <santoshreddy119@gmail.com>
2018-01-10 22:46:12 +0530

    Test branch commit

--- src/main/java/hello/Person.java
+++ src/main/java/hello/Person.java
@@ -8,6 +8,24 @@ public class Person {
 
     private String firstName;
     private String lastName;
+    private String mobileNumber;
+    private String address;
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public String getFirstName() {
         return firstName;
